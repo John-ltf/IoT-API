9c8509d7a4b22fcfb151d958b9cf0e980f1686a0
Latifis Giannis <latifis.giannis@gmail.com>
2022-10-26 14:26:40 +0300

    2

--- IoT-web-api/Startup.cs
+++ IoT-web-api/Startup.cs
@@ -43,10 +43,10 @@ namespace IoT_API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
 
